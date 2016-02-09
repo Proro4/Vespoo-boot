56641d823d1ae8e8661106689e82c21ff7c15eb8
Proro4 <proro4@gmail.com>
2016-02-09 13:56:26 +0200

    v0.40.1

--- js/lightbox.js
+++ js/lightbox.js
@@ -90,28 +90,110 @@ $(document).ready(function(){
 //DnD
 
 $(document).ready(function(){
-	setInterval(function(){
-		$(".dz-image img").attr('draggable','true');
-	},3000)
+	// setInterval(function(){
+	// 	$(".photoload-in").attr('draggable','true');
+	// },3000)
   
-  $(".dz-image").on("dragover",function(e){
-  	console.log('1');
-    e.preventDefault();
-  });
+  // $(".dz-image").on("dragover",function(e){
+  // 	console.log('1');
+  //   e.preventDefault();
+  // });
   
-  $(".dz-image img").on("dragstart",function(e){
-    e.originalEvent.dataTransfer.setData("Text",e.target.id);
-  });
+  // $(".dz-image img").on("dragstart",function(e){
+  //   e.originalEvent.dataTransfer.setData("Text",e.target.id);
+  // });
   
   
-  $(".photoload-in").on("drop",function(e){
-  	alert('1');
-  	console.log('2');
-    e.preventDefault();
-    var data=e.originalEvent.dataTransfer.getData("Text");
-    e.target.appendChild(document.getElementById(data));
-  });
+  // $(".photoload-in").on("drop",function(e){
+  // 	// alert('1');
+  // 	console.log('2');
+  //   e.preventDefault();
+  //   var data=e.originalEvent.dataTransfer.getData("Text");
+  //   e.target.appendChild(document.getElementById(data));
+  // });
   
 
-  
+	
+
+
+
+
+
+
+
+
+
+
+
+  function handleDragOver(e) {
+	  if (e.preventDefault) {
+	    e.preventDefault(); // Necessary. Allows us to drop.
+	  }
+
+	  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+	  return false;
+	}
+
+	function handleDragEnter(e) {
+	  // this / e.target is the current hover target.
+	  this.classList.add('over');
+	}
+
+	function handleDragLeave(e) {
+	  this.classList.remove('over');  // this / e.target is previous target element.
+	}
+
+	var cols = document.querySelectorAll('.photoload .dropzone');
+	[].forEach.call(cols, function(col) {
+	  col.addEventListener('dragstart', handleDragStart, false);
+	  col.addEventListener('dragenter', handleDragEnter, false);
+	  col.addEventListener('dragover', handleDragOver, false);
+	  col.addEventListener('dragleave', handleDragLeave, false);
+	});
+
+	function handleDrop(e) {
+	  // this / e.target is current target element.
+
+	  if (e.stopPropagation) {
+	    e.stopPropagation(); // Stops some browsers from redirecting.
+	  }
+
+	  // Don't do anything if dropping the same column we're dragging.
+	  if (dragSrcEl != this) {
+	    // Set the source column's HTML to the HTML of the columnwe dropped on.
+	    dragSrcEl.innerHTML = this.innerHTML;
+	    this.innerHTML = e.dataTransfer.getData('text/html');
+	  }
+
+	  return false;
+	}
+
+	function handleDragEnd(e) {
+	  // this/e.target is the source node.
+
+	  [].forEach.call(cols, function (col) {
+	    col.classList.remove('over');
+	  });
+	}
+
+	var cols = document.querySelectorAll('.photoload .dropzone');
+	[].forEach.call(cols, function(col) {
+	  col.addEventListener('dragstart', handleDragStart, false);
+	  col.addEventListener('dragenter', handleDragEnter, false)
+	  col.addEventListener('dragover', handleDragOver, false);
+	  col.addEventListener('dragleave', handleDragLeave, false);
+	  col.addEventListener('drop', handleDrop, false);
+	  col.addEventListener('dragend', handleDragEnd, false);
+	});
+	var dragSrcEl = null;
+
+	function handleDragStart(e) {
+	  dragSrcEl = this;
+	  e.dataTransfer.effectAllowed = 'move';
+	  e.dataTransfer.setData('text/html', this.innerHTML);
+	}
+
+	  	
 });
+
